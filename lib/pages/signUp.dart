6e3b02fd6fbfb9a480891fb1182476ab663fa7d9
mohamed diop <mouh09diope@gmail.com.com>
2023-06-24 19:34:53 +0100

    first commit

---
+++ lib/pages/signUp.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SignUp extends StatefulWidget {
+  const SignUp({super.key});
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+            children: [
+              SizedBox(height: 60,)
+            ],
+        ),
+      ),
+    );
+  }
+}
